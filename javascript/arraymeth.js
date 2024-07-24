e10ef047e6644c2ef4d55b93841c2d3cb0382abb
Ayush Nagdeve <88583456+ayu2kk2@users.noreply.github.com>
2024-07-24 20:07:40 +0000

    config confirm

---
+++ javascript/arraymeth.js
@@ -0,0 +1,33 @@
+// Array methods
+//convert array into string
+let a = [1,2,3,4,5,78,67]
+
+let b = a.toString()
+console.log(b)
+console.log(a)//it will print as a string
+
+let c = a.join("_")
+console.log(c, typeof c)
+
+//let yu = a.pop() // remove last element
+//console.log(a, yu)
+
+//let y = a.push(89)
+//console.log(a, y)// it returns array length
+
+//let o = a.shift() // remove first element and returns it 
+//console.log(a, o)
+
+//let o1 = a.unshift(23) // add at beginning and returns the legthn of array
+//console.log(a,o1)
+
+
+//concat in array
+let u = a.concat(26,43,45)
+console.log(a, u)
+//console.log(a.concat(26,43,45))
+
+
+
+
+
